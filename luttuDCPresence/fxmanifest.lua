e0d39aa3cf567968697067ca9303b24a1111222a
RD Luttu <33906124+AnandBArjun@users.noreply.github.com>
2022-06-05 13:14:05 +0530

    Add files via upload

---
+++ luttuDCPresence/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+name 'LuttuRichPresence'
+
+version '1.0'
+
+author 'RD Luttu#5198'
+
+
+server_script {
+    'config.lua',
+    'server/main.lua'
+}
+
+client_script {
+    'config.lua',
+    'client/main.lua'
+}
